7c2cf50023d6e9cbb3b1270df473f3a6ed0c86f4
chaseseidel <chaseseidel16@gmail.com>
2023-06-05 13:34:33 -0700

    Add working display and reset button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-//Variables
-let firstNumber;
-let secondNumber;
-let operator;
-
 //Calculator functions
 const add = function(a, b) {
     return a + b;
@@ -22,12 +17,62 @@ const divide = function(a, b) {
 
 const operate = function(operator, a, b) {
     if (operator == 'add') {
-        return add(a, b);
+        firstNumber = add(a, b);
+        secondNumber = null;
+        displayVariable = add(a, b);
+        updateDisplay();
     } else if (operator == 'subtract') {
-        return subtract(a, b);
+        firstNumber = subtract(a, b);
+        secondNumber = null;
+        displayVariable = subtract(a, b);
+        updateDisplay();
     } else if (operator == 'multiply') {
-        return multiply(a, b);
+        firstNumber = multiply(a, b);
+        secondNumber = null;
+        displayVariable = multiply(a, b);
+        updateDisplay();
     } else if (operator == 'divide') {
-        return divide(a, b);
+        firstNumber = divide(a, b);
+        secondNumber = null;
+        displayVariable = divide(a, b);
+        updateDisplay();
     }
 }
+//number variables
+let firstNumber;
+let secondNumber;
+
+//display
+let displayVariable;
+const display = document.querySelector('.display');
+function updateDisplay() {
+    display.textContent = displayVariable;
+}
+
+//digit buttons
+const digits = document.querySelectorAll('.digit');
+digits.forEach(digit => digit.addEventListener('click', () => {
+    displayVariable = digit.textContent;
+    updateDisplay();
+    !firstNumber || !operatorVariable ? firstNumber = parseInt(digit.textContent) 
+    : secondNumber = parseInt(digit.textContent);
+}))
+
+//operator buttons
+let operatorVariable;
+const operators = document.querySelectorAll('.operator');
+operators.forEach(operator => operator.addEventListener('click', () => {
+    operatorVariable = operator.id;
+}))
+const equals = document.getElementById('equals');
+equals.addEventListener('click', () => operate(operatorVariable, firstNumber, secondNumber));
+
+//top buttons
+const clear = document.getElementById('clear');
+clear.addEventListener('click', () => {
+    firstNumber = null;
+    secondNumber = null;
+    displayVariable = 0;
+    operatorVariable = null;
+    updateDisplay();
+})
